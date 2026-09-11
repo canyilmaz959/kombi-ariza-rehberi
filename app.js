7e9f05677935e21fd542045e14eb462a29de15bc
Can <muhammetcanyilmaz959@gmail.com>
2026-09-11 20:59:11 +0300

    feat: kullanıcı ve admin sayfalarını yeniden tasarla - Ortak responsive tasarım sistemi eklendi - Ana sayfa ve arama sonuçları yenilendi - Marka, model ve arıza sayfaları tasarlandı - Admin giriş ekranı modernleştirildi - Admin paneli kart tabanlı forma dönüştürüldü - Arıza listesi ve düzenleme ekranları yenilendi - Ortak navigasyon ve sayfalama bileşeni eklendi - Public CSS dosyalarının Express üzerinden sunulması sağlandı - Mobil uyumluluk iyileştirildi

--- app.js
+++ app.js
@@ -48,6 +48,7 @@ async function startServer() {
 //ejs ayarları
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.urlencoded({ extended: true }));
 
 
